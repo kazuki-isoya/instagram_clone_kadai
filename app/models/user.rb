efc2db66bf7dfd781106a686f4bdc6f34d6692f9
moru more <moremoru.world21@gmail.com>
2020-01-24 16:59:37 +0900

    バリデーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validate :name, presence: true, lenght: { maximum: 30}
+  validate :email. presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
