23568a718e09a08bc74238389347b84bdc52483f
moru more <moremoru.world21@gmail.com>
2020-01-24 17:41:55 +0900

    user新規登録フォームを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validate :name, presence: true, lenght: { maximum: 30}
-  validate :email. presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, presence: true, length: { maximum: 30}
+  validates :email, presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase!}
   has_secure_password
-  validate :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
