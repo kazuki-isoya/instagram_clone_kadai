d23ec5eb85c614e7a667748e325553335a8fc813
moru more <moremoru.world21@gmail.com>
2020-01-24 17:18:20 +0900

    パスワードの暗号化とバリデーションの追記　gemを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validate :name, presence: true, lenght: { maximum: 30}
   validate :email. presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase!}
+  has_secure_password
+  validate :password, presence: true, length: { minimum: 6 }
 end
