27a4f3734b63b167b9872d5955bb2a4ba9cd8c1a
moru more <moremoru.world21@gmail.com>
2020-01-24 17:10:12 +0900

    emailを小文字に変換するように記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validate :name, presence: true, lenght: { maximum: 30}
   validate :email. presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase!}
 end
