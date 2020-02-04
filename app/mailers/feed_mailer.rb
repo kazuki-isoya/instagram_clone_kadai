efadb310bc88ce269c8897ec59ac93cae061ba88
moru more <moremoru.world21@gmail.com>
2020-02-04 10:32:09 +0900

    heroku、本番環境でのメーラーを設定

--- app/mailers/feed_mailer.rb
+++ app/mailers/feed_mailer.rb
@@ -2,6 +2,6 @@ class FeedMailer < ApplicationMailer
   def feed_mail(feed)
     @feed = feed
 
-    mail to:"moremoru.world21@gmail.com", subject:"お問い合わせの確認メール"
+    mail to: "moremoru.world21@gmail.com", subject:"お問い合わせの確認メール"
   end
 end
