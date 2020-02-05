ce2d4a21725bde08a3e800b5c5daa03c6ea8ee2c
moru more <moremoru.world21@gmail.com>
2020-02-05 09:49:14 +0900

    favoriteモデルを作成　userとfeedに関連付けをした

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,6 +1,8 @@
 class Feed < ApplicationRecord
   mount_uploader :image, ImageUploader
-  belongs_to :user
+  has_many :favorites
+  has_many :users, through: :favorites
+  
   validates :image, presence: true
   validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30}
   validates :email, presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase!}
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   has_many :feeds
+  has_many :favorites
+  has_many :fav_feeds, through: :favorites, source: :feed
+
   mount_uploader :image, ImageUploader
 end
