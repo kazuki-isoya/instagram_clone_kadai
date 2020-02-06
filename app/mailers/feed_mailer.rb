0ddb94de1907845a093e08bd8363a3f5fd0e0a6c
moru more <moremoru.world21@gmail.com>
2020-02-06 13:40:35 +0900

    feedメーラーのコメント削除

--- app/mailers/feed_mailer.rb
+++ app/mailers/feed_mailer.rb
@@ -4,10 +4,3 @@ class FeedMailer < ApplicationMailer
     mail to: "@feed.user.email", subject:"投稿の確認メール"
   end
 end
-
-# 元のコード
-# def feed_mail(feed)
-#   @feed = feed
-#
-#   mail to: "moremoru.world21@gmail.com", subject:"投稿の確認メール"
-# end
