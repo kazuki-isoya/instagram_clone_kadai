cefa1a0207f35d388d38ce8afbeff80c5c603fa4
moru more <moremoru.world21@gmail.com>
2020-02-06 20:47:11 +0900

    メールが届かない箇所を修正

--- app/mailers/feed_mailer.rb
+++ app/mailers/feed_mailer.rb
@@ -1,6 +1,6 @@
 class FeedMailer < ApplicationMailer
   def feed_mail(feed)
     @feed = feed
-    mail to: "@feed.user.email", subject:"投稿の確認メール"
+    mail to: @feed.user.email, subject:"投稿の確認メール"
   end
 end
