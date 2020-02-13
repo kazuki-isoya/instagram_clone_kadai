1c87a4c50fd292f790a2af7744924b5ad2a48060
moru more <moremoru.world21@gmail.com>
2020-02-13 10:50:58 +0900

    coversationモデルとmessageモデルを作った

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_035218) do
+ActiveRecord::Schema.define(version: 2020_02_13_014418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id", "recipient_id"], name: "index_conversations_on_sender_id_and_recipient_id", unique: true
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id"
@@ -32,6 +42,17 @@ ActiveRecord::Schema.define(version: 2020_02_12_035218) do
     t.bigint "user_id"
     t.text "content"
     t.index ["user_id"], name: "index_feeds_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.boolean "read", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -66,4 +87,6 @@ ActiveRecord::Schema.define(version: 2020_02_12_035218) do
   add_foreign_key "favorites", "feeds"
   add_foreign_key "favorites", "users"
   add_foreign_key "feeds", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
