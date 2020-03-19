c6ba95d4a37224728db6036fc02df85e309941bb
moru more <moremoru.world21@gmail.com>
2020-03-19 17:29:35 +0900

    awsのs3用に設定を変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :thumb50 do
     process resize_to_fit: [100, 100]
   end
-  storage :file
+  storage :fog
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
